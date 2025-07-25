ef3743577b7e2eb4abf368604961d67fb51c30db
Abusolay <sefat01625@gmail.com>
2025-07-26 02:35:54 +0600

    socal login

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        child_process: false,
+        tls: false,
+        net: false,
+      };
+    }
+    return config;
   },
 };
 
